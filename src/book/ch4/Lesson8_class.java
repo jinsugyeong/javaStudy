f5dfed90e099dd7dee067b9930e036f7a4d035fb
jinsugyeong <apr15th@naver.com>
2021-12-26 17:21:15 +0900

    그림으로 배우는 JAVA 프로그래밍 BASIC 클래스

---
+++ src/book/ch4/Lesson8_class.java
@@ -0,0 +1,31 @@
+package book.ch4;
+
+class MyPoint {
+	private int x;
+	private int y;
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}
+
+public class Lesson8_class {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
